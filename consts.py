84fa0f765ad446fda04829906091bb06420ef971
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-10-01 23:22:50 +0530

    header

---
+++ consts.py
@@ -0,0 +1,11 @@
+class Fonts:
+    _16 = ("Consolas", 16)
+    _20 = ("Consolas", 20)
+    _40 = ("Consolas", 40)
+    _48 = ("Consolas", 48)
+
+class Colors:
+    primary = "#262626"
+    secondary = "#353535"
+    accent = "#282828"
+    text = "#ffffff"
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import tkinter as tk
-import tkinter.font as font
+from consts import Fonts
+from consts import Colors
 
 class App:
     def __init__(self, root: tk.Tk) -> None:
         self.root = root
         self.root.geometry("500x800")
         self.root.resizable(False, False)
-        self.root.configure(background="#262626")
+        self.root.configure(background=Colors.primary)
 
+        self.time = tk.StringVar(self.root, "00-00-0000 00:00 PM")
+        self.timer_var = tk.StringVar(self.root, "00:00:00")
+
+        self.clock = tk.Label(self.root, textvariable=self.time, font=Fonts._20, bg=Colors.primary, fg=Colors.text)
+        self.clock.place(x=0, y=20, width=500, height=25)
+
+        tk.Label(self.root, text="Total", font=Fonts._16, bg=Colors.primary, fg=Colors.text).place(x=232, y=65)
+
+        self.grandtimer = tk.Label(self.root, textvariable=self.timer_var, font=Fonts._48, bg=Colors.primary, fg=Colors.text)
+        self.grandtimer.place(x=0, y=100, width=500, height=55)
 
 if __name__ == "__main__":
     root = tk.Tk()
