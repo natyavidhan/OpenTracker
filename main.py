2036b7e60c1ec92da3813aee30db98dd393f1ffe
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-10-02 00:17:31 +0530

    imgs

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class App:
         self.root.configure(background=Colors.primary)
 
         self.settings = json.load(open("settings.json"))
+        self.start_img = tk.PhotoImage(file="assets/start.png")
+        self.root.image = self.start_img
+        self.pause_img = tk.PhotoImage(file="assets/pause.png")
+        self.root.image = self.pause_img
 
         self.time = tk.StringVar(self.root, "00-00-0000 00:00 PM")
         self.timer_var = tk.StringVar(self.root, "00:00:00")
@@ -27,23 +31,25 @@ class App:
         self.grand_timer = tk.Label(self.root, textvariable=self.timer_var, font=Fonts._48, bg=Colors.primary, fg=Colors.text)
         self.grand_timer.place(x=0, y=100, width=500, height=55)
 
-        self.tabs_frame = tk.Frame(self.root, width=450, height=500)
+        self.tabs_frame = tk.Frame(self.root, width=450, height=500, borderwidth=0, highlightthickness=0)
         self.tabs_frame.place(x=25, y=165)
-        
             
-        self.timer_canvas = tk.Canvas(self.tabs_frame)
+        self.timer_canvas = tk.Canvas(self.tabs_frame, bg=Colors.secondary, borderwidth=0, highlightthickness=0)
+
         vbar=tk.Scrollbar(self.timer_canvas, orient=VERTICAL)
         vbar.pack(side=RIGHT,fill=Y)
         vbar.config(command=self.timer_canvas.yview)
+
         self.timer_canvas.config(yscrollcommand=vbar.set)
         self.timer_canvas.place(x=0, y=0, width=450, height=500)
 
         self.subs_frames = []
 
         for idx, subs in enumerate(self.settings['subjects']):
-            self.timer_canvas.create_rectangle(25, 20+(120*idx), 425, 120+(120*idx))
+            self.timer_canvas.create_rectangle(25, 20+(120*idx), 425, 120+(120*idx), fill=Colors.primary, width=0)
             self.timer_canvas.config(scrollregion=(0, 0, 450, 140+(120*idx)))
 
+            self.timer_canvas.create_image((40, 35+(120*idx)), anchor="nw", image=self.start_img)
 
 if __name__ == "__main__":
     root = tk.Tk()
